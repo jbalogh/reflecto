103eaf27a715e9241a64f6b11d8978f6c8548ee6
Jeremy Orem <jeremy.orem@gmail.com>
2011-04-25 16:49:33 -0700

    repo list

--- reflecto.py
+++ reflecto.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import logging
 import logging.handlers
@@ -29,10 +30,53 @@ def create_or_update_repo(url, path):
         subprocess.Popen([GIT, 'fetch'], cwd=target)
 
 
+def get_repo_config(dir, key):
+    p = subprocess.Popen([GIT, 'config', '--get', key],
+                            stdout=subprocess.PIPE, cwd=dir)
+    value = p.communicate()[0].strip()
+    return value
+
+
+def repo_list():
+
+    def to_url(url, rev):
+        m = re.search('(.+/(.+/.+))\.git', url)
+        if m:
+            base = m.group(1)
+            name = m.group(2)
+            if rev:
+                return '<a href="{0}/commit/{1}">{2} ({1})</a>'.format(base, rev[:6], name)
+            else:
+                return '<a href="{0}">{1}</a>'.format(base, name)
+
+
+    def get_latest_rev(r):
+        master_ref_path = os.path.join(r, 'refs', 'heads', 'master')
+        try:
+            return open(master_ref_path).read().strip()
+        except IOError:
+            return ''
+
+
+    def repo_urls():
+        repos = glob.glob(os.path.join(ROOT, "*", "*"))
+        for r in repos:
+            url = get_repo_config(r, 'remote.origin.url') 
+            rev = get_latest_rev(r)
+            yield to_url(url, rev)
+
+
+    def to_ul(items):
+        return '<ul>%s</ul>' % "\n".join('<li>%s</li>' % u for u in items)
+
+
+    return to_ul(repo_urls())
+
+
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
+    start_response('200 OK', [('Content-Type', 'text/html')])
     if env['REQUEST_METHOD'] != 'POST':
-        return ''
+        return [repo_list()]
 
     payload = dict(urlparse.parse_qsl(env['wsgi.input'].read()))['payload']
     repo = json.loads(payload)['repository']
